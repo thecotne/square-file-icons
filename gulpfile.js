7b01cb125a5c0b2d4cd025082138baa84d3b3376
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2015-09-03 15:36:49 +0400

    add woff2 support and did some refactoring fixes #8

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,80 @@ var consolidate = require('gulp-consolidate');
 var rename = require("gulp-rename");
 var minifyCss = require('gulp-minify-css');
 
+
+function endsWith(str, suffix) {
+	return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+function cutOff(str, length) {
+	return str.substring(0, str.length - length);
+}
+function glyphReverseName(name) {
+	if (endsWith(name, '-o')) {
+		return cutOff(name, 2)
+	} else {
+		return name + '-o';
+	}
+}
+function find(arr, fn) {
+	for(var i in arr) {
+		if (fn(arr[i])) {
+			return arr[i];
+		};
+	}
+}
+
+function transformGlyphs(glyphs) {
+	return glyphs.map(function(glyph) {
+		switch(glyph.name) {
+			case 'excel':
+			case 'excel-o':
+				glyph.color = '#08743b';
+				break;
+			case 'image':
+			case 'image-o':
+				glyph.color = '#5b2d8d';
+				break;
+			case 'pdf':
+			case 'pdf-o':
+				glyph.color = '#EA4C3A';
+				break;
+			case 'powerpoint':
+			case 'powerpoint-o':
+				glyph.color = '#DD5B26';
+				break;
+			case 'text':
+			case 'text-o':
+				glyph.color = '#6B533B';
+				break;
+			case 'word':
+			case 'word-o':
+				glyph.color = '#2372BA';
+				break;
+		}
+
+		var revreseName = glyphReverseName(glyph.name);
+
+		glyph.reverseGlyph = find(glyphs, function(val) {
+			return val.name == revreseName;
+		});
+
+		return glyph;
+	});
+}
+
 gulp.task('default', function () {
 	gulp.src(['icons/*.svg'])
 		.pipe(iconfont({
-			fontName: 'square-file'
+			fontName: 'square-file',
+			formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
 		}))
-		.on('codepoints', function (codepoints, options) {
-			var glyphs = {};
-			codepoints.forEach(function(glyph) {
-				glyphs[glyph.name] = '\\'+glyph.codepoint.toString(16).toUpperCase()
-			});
+		.on('glyphs', function (glyphs, options) {
 			gulp.src('template.css')
 				.pipe(consolidate('lodash', {
-					glyphs: glyphs,
+					glyphs: transformGlyphs(glyphs),
 					fontName: 'square-file',
 					fontPath: '../fonts/',
 					className: 'sf',
-					normalize: true,
 				}))
 				.pipe(rename({basename: 'square-file'}))
 				.pipe(gulp.dest('dist/css/'))
